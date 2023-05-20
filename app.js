8094acf93b322f01ce355414f52930fc23ef8199
Ranger-Jay <68936288+Ranger-Jay@users.noreply.github.com>
2023-05-19 23:39:32 -0400

    pieces added and gameboard made

--- app.js
+++ app.js
@@ -4,5 +4,31 @@ const infoDisplay = document.querySelector("#info-display");
 
 const width = 8
 const startPieces = [
-
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
 ]
+
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.innerHTML = startPiece
+        square.setAttribute('square-id', i)
+        // square.classList.add('beige')
+        const row = Math.floor( (63 - i) / 8) + 1
+        if (row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? "beige" : "brown")
+        } else {
+            square.classList.add(i % 2 === 0 ? "brown" : "beige")
+        }
+        gameBoard.append(square)
+    })
+}
+
+createBoard()
