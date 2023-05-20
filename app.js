1e3c8a640fe74233bd16318821686de313e7001d
Ranger-Jay <68936288+Ranger-Jay@users.noreply.github.com>
2023-05-20 03:17:30 -0400

    turn order functions fix

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const gameBoard = document.querySelector("#gameboard");
 const playerDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("#info-display");
-
 const width = 8
+let playerGo = 'black'
+playerDisplay.textContent = 'black'
+
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
@@ -14,7 +16,7 @@ const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook
 ]
 
-function createBoard() {
+function createBoard() { //handles initial layout of gameboard
     startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div')
         square.classList.add('square')
@@ -38,18 +40,18 @@ function createBoard() {
 }
 createBoard()
 
-const allSquares = document.querySelectorAll("#gameboard .square")
+const allSquares = document.querySelectorAll(".square")
 
-allSquares.forEach(square => {
+allSquares.forEach(square => { //handles the movement, check the css to understand
     square.addEventListener('dragstart', dragStart)
     square.addEventListener('dragover', dragOver)
-    square.addEventListener('drop', dragDrop)
+    square.addEventListener('drop', dragDrop) //the magic happens here
 })
 
 let startPositionId
 let draggedElement
 
-function dragStart (e) {
+function dragStart (e) { //target derived from console log events
     startPositionId = e.target.parentNode.getAttribute('square-id')
     draggedElement= e.target
 }
@@ -58,9 +60,36 @@ function dragOver(e) {
     e.preventDefault()
 }
 
-function dragDrop(e) {
+function dragDrop(e) { //the magic
      e.stopPropagation()
+    const taken = e.target.classList.contains('piece')
 
     //  e.target.parentNode.append(draggedElement)
-    e.target.append(draggedElement)
+    //  e.target.remove()
+    // e.target.append(draggedElement)
+    changePlayer()    
+}
+
+function changePlayer() { //handles turn order under board (flip squareIDs)
+    if(playerGo === "black") {
+        reverseIds()
+        playerGo = "white"
+        playerDisplay.textContent = 'white'
+    } else {
+        revertIds()
+        playerGo = "black"
+        playerDisplay.textContent = 'black'
+    }
+}
+
+function reverseIds() { //reverses board square-IDs for turn handling
+    const allSquares = document.querySelectorAll(".square")
+    allSquares.forEach((square, i) =>
+        square.setAttribute('square-id', (width * width -1) - i)) //cool way to do reverse
+}
+
+function revertIds() { //sets IDs back to white after black
+    const allSquares = document.querySelectorAll(".square")
+    allSquares.forEach((square, i) =>
+        square.setAttribute('square-id', i))
 }
